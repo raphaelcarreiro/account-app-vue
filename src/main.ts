74ea8337a4b573e9b951bee43632df6285b1d773
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-08-11 09:12:30 -0300

    refactor: remove unused ElementPlusIconsVue imports and registration

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,9 @@ import 'element-plus/theme-chalk/dark/css-vars.css';
 import './styles/discord.css';
 import './styles/global.css';
 
-import * as ElementPlusIconsVue from '@element-plus/icons-vue';
-
 const app = createApp(App);
 
 app.use(router);
 app.use(ElementPlus, { locale: ptBR, size: 'large' });
-
-for (const [name, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(name, component);
-}
 
 app.mount('#app');
